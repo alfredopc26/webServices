ce22ce9fb2948223347b20b388e75eb6b8dd68b4
alfredopc26 <calfredoperez26@gmail.com>
2020-05-25 14:35:31 -0500

    UpdateCentro_salud

--- script/centro_salud.sql
+++ script/centro_salud.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 13-05-2020 a las 04:25:22
+-- Tiempo de generación: 25-05-2020 a las 21:33:09
 -- Versión del servidor: 10.4.11-MariaDB
 -- Versión de PHP: 7.4.5
 
@@ -28,6 +28,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `doctor` (
+  `id` int(11) NOT NULL,
   `hospital_ID` int(11) NOT NULL,
   `nombre` varchar(50) NOT NULL,
   `direccion` varchar(25) NOT NULL,
@@ -36,6 +37,15 @@ CREATE TABLE `doctor` (
   `experiencia` int(11) NOT NULL,
   `nacimiento` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `doctor`
+--
+
+INSERT INTO `doctor` (`id`, `hospital_ID`, `nombre`, `direccion`, `telefono`, `tipo_sangre`, `experiencia`, `nacimiento`) VALUES
+(7, 1, 'Jaime Charris', 'Cra 54 #56-33', 3057993, 'A+', 5, '1991-03-31'),
+(8, 1, 'Marina Zuleta', 'Cra 44 #72-29', 4321873, 'O-', 6, '1992-06-13'),
+(9, 7, 'Gustavo Perez', 'cra 29 99 24', 214748364, 'A+', 10, '1992-07-22');
 
 -- --------------------------------------------------------
 
@@ -57,7 +67,22 @@ CREATE TABLE `hospital` (
 --
 
 INSERT INTO `hospital` (`id`, `nombre`, `telefono`, `direccion`, `nit`, `representante`) VALUES
-(1, 'Hospital CAP', 3069811, 'cra 29 99 24', 1051418846, 'Carlos Perez');
+(1, 'Hospital CAP', 3069811, 'cra 29 99 24', 1051418846, 'Carlos Perez'),
+(2, 'Perez Hospital', 3000026, 'cra 25 12-43', 802000989, 'Alfredo Cassiani'),
+(7, 'Gustavo Perez', 3000256, 'Cra 56 #87-45', 4886524, 'Gustavo Perez');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `mascotas`
+--
+
+CREATE TABLE `mascotas` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(25) NOT NULL,
+  `raza` varchar(50) NOT NULL,
+  `edad` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -67,6 +92,7 @@ INSERT INTO `hospital` (`id`, `nombre`, `telefono`, `direccion`, `nit`, `represe
 
 CREATE TABLE `paciente` (
   `identificacion` varchar(10) NOT NULL,
+  `hospital_ID` int(11) NOT NULL,
   `nombre` varchar(50) NOT NULL,
   `eps` varchar(25) NOT NULL,
   `direccion` varchar(50) NOT NULL,
@@ -79,10 +105,8 @@ CREATE TABLE `paciente` (
 -- Volcado de datos para la tabla `paciente`
 --
 
-INSERT INTO `paciente` (`identificacion`, `nombre`, `eps`, `direccion`, `nombre_a`, `telefono_a`, `antecedentes`) VALUES
-('1051418846', 'carlos perez', 'sura', 'cra 29 99 24', 'karen venera', '3002193470', 'N'),
-('1065214582', 'Linda Daza', 'Coomeva', 'Calle 123', 'juan', '300219039', 'N'),
-('1065829578', 'karen Venera', 'Sanitas', 'av 123', 'Carlos', '300219', 'N');
+INSERT INTO `paciente` (`identificacion`, `hospital_ID`, `nombre`, `eps`, `direccion`, `nombre_a`, `telefono_a`, `antecedentes`) VALUES
+('1051418846', 7, 'Carlos Perez', 'sura', 'Cra45-8739av2', 'Andrea Jimenez', '3002193470', 'N');
 
 -- --------------------------------------------------------
 
@@ -92,14 +116,24 @@ INSERT INTO `paciente` (`identificacion`, `nombre`, `eps`, `direccion`, `nombre_
 
 CREATE TABLE `triage` (
   `id` int(11) NOT NULL,
+  `hospital_ID` int(11) NOT NULL,
   `doctorID` int(10) NOT NULL,
   `pacienteID` int(10) NOT NULL,
   `motivos_consulta` varchar(250) NOT NULL,
   `diagnostico` varchar(250) NOT NULL,
   `req_medicamento` varchar(1) NOT NULL,
   `medicamento` varchar(250) NOT NULL,
+  `sintomas` varchar(100) NOT NULL,
   `pos_COVID19` varchar(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `triage`
+--
+
+INSERT INTO `triage` (`id`, `hospital_ID`, `doctorID`, `pacienteID`, `motivos_consulta`, `diagnostico`, `req_medicamento`, `medicamento`, `sintomas`, `pos_COVID19`) VALUES
+(10, 1, 7, 105427837, 'Dolor de muela', 'Inflamacion en las muelas cordales', 'Y', 'DIclofenaco, Acetaminofen', 'Fiebre,', 'N'),
+(11, 7, 9, 1051418846, 'Dolor en la muel', 'Inflamacion severa', 'Y', 'Diclofenaco, Acetaminofen', 'Dolor de Garganta,Fiebre,Tos,', 'Y');
 
 --
 -- Índices para tablas volcadas
@@ -109,12 +143,18 @@ CREATE TABLE `triage` (
 -- Indices de la tabla `doctor`
 --
 ALTER TABLE `doctor`
-  ADD PRIMARY KEY (`nombre`,`hospital_ID`);
+  ADD PRIMARY KEY (`id`) USING BTREE;
 
 --
 -- Indices de la tabla `hospital`
 --
 ALTER TABLE `hospital`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `mascotas`
+--
+ALTER TABLE `mascotas`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -134,16 +174,28 @@ ALTER TABLE `triage`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `doctor`
+--
+ALTER TABLE `doctor`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
 -- AUTO_INCREMENT de la tabla `hospital`
 --
 ALTER TABLE `hospital`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de la tabla `mascotas`
+--
+ALTER TABLE `mascotas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `triage`
 --
 ALTER TABLE `triage`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
